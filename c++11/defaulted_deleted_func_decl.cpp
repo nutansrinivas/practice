3bfb80c8e164f7e34cf483a29e79b0cf2f7ff8c2
Srinivas <srinivas@localhost.localdomain>
2021-12-28 23:08:07 +0530

    defaulted description added

---
+++ c++11/defaulted_deleted_func_decl.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+//though special member functions like constructor, copy constructor defined explicitly, to include defaulted member functions, shall delcare the api with defaulted kyword
+//for example, to include default constructor of class Test,  Test() = default; or copy constructor Test(const Test&) = default;
+
+
+//
